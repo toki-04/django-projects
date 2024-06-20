95f909c206dd32fd7ccc25b6531754af8fe3246e
toki-04 <toki.cc.04@gmail.com>
2024-06-20 21:33:33 +0800

    Post Template

--- social-media/community/views.py
+++ social-media/community/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 
 @login_required(login_url="/login/")
 def community(request):
-    return render(request, "community/community.html")
+    posts = Post.objects.all()
+
+    context = {
+        "posts": posts,
+    }
+
+    return render(request, "community/community.html", context)
