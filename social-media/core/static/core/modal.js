11b374f91ac4c063492288e921fca4fe51defbc0
toki-04 <toki.cc.04@gmail.com>
2024-06-22 09:47:26 +0800

    Modal Bug Fix

--- social-media/core/static/core/modal.js
+++ social-media/core/static/core/modal.js
@@ -8,3 +8,13 @@ function open_modal(){
   modal.style.display = "flex"
   document.body.className = "stop-scrolling"
 }
+
+function modal_preview_image(){
+  const image = document.getElementById("modal-upload-image")
+  const preview_img = document.getElementById("modal-preview-img")
+
+  const [file] = image.files
+
+  preview_img.src = URL.createObjectURL(file)
+  preview_img.style.display = "block"
+}
