6cff6d652362ba8b9a3d4e6a7278015e807cf3ad
toki-04 <toki.cc.04@gmail.com>
2024-06-25 04:35:38 +0800

    Dashboard

--- social-media/dashboard/views.py
+++ social-media/dashboard/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from user_profile.models import Profile
 
 # Create your views here.
 
 
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user=request.user.id)
+
+    else:
+        profile = ""
+
+    context = {
+        "profile": profile
+    }
+    return render(request, "dashboard/dashboard.html", context)
