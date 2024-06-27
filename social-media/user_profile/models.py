56af4dc749ab7d0942efb02f5e4d282a80502cc3
toki-04 <toki.cc.04@gmail.com>
2024-06-27 21:58:57 +0800

    user profile and dashboard updated

--- social-media/user_profile/models.py
+++ social-media/user_profile/models.py
@@ -8,17 +8,13 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile = models.ImageField(upload_to="images/profile/", blank=True)
+    about_me = models.CharField(max_length=1000, blank=True)
     followers = models.ManyToManyField(
         "self",
         related_name="followed_by",
         symmetrical=False,
         blank=True,
     )
-    # posts = models.ManyToManyField(
-    # Post,
-    # symmetrical=False,
-    # blank=True,
-    # )
 
     def __str__(self):
         return self.user.username
